c73070d4c58ae4253c0ca3f2ec8f997bc8c6538b
Brandon Roberts <brandonlroberts@outlook.com>
2023-01-30 16:26:35 -0500

    I think it works... Needs some serious refactoring

---
+++ CustomDateTimePicker/PickerConstants.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomDateTimePicker
+{
+    public static class PickerConstants
+    {
+        public static readonly string MaximumYearsString = "2030";
+        public static readonly string MinimumYearsString = "2020";
+        public static readonly string MaximumMonthsString = "12";
+        public static readonly string MaximumDaysString = "31";
+        public static readonly string MaximumHoursString = "23";
+        public static readonly string MaximumMinutesString = "59";
+
+        public static readonly string Month = "Month";
+        public static readonly string Date = "Date";
+        public static readonly string Year = "Year";
+        public static readonly string Hour = "Hour";
+        public static readonly string Minute = "Minute";
+
+        public static readonly int MaximumYears = 2030;
+        public static readonly int MinimumYears = 2000;
+        public static readonly int MaximumMonths = 12;
+        public static readonly int MaximumDays = 31;
+        public static readonly int MaximumHours = 23;
+        public static readonly int MaximumMinutes = 59;
+        
+        public static List<string> numbers = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+    }
+}
